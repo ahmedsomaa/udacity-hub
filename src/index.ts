20a94d70516363de369ab10041fcb8278a3d27ef
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-10-15 02:29:16 +0200

    feat (index): Use express.json & application routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import config from './config';
 import express from 'express';
+import routes from './routes';
 
 const { port } = config;
 
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
