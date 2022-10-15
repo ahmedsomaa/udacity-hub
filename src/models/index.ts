23ee85cff1255e08f14870e12ce7613bc0e53ba3
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-10-15 02:30:16 +0200

    fix (models): Update Student type and add SessionStudent type

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,15 +1,22 @@
 export type SessionLead = {
-    id?: number;
-    name: string;
+  id?: number;
+  name: string;
 };
 
 export type Student = {
-    id?: number;
-    name: string;
+  id?: number;
+  name: string;
+  session_lead_id: number;
 };
 
 export type Session = {
-    id?: number;
-    title: string;
-    session_date: Date;
+  id?: number;
+  title: string;
+  session_date: Date;
+};
+
+export type SessionStudent = {
+  id?: number;
+  session_id: number;
+  student_id: number;
 };
